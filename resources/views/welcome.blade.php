ebf23d950584712a6518966cff216b03371658f4
Pratikupreti8 <pratikupreti419@gmail.com>
2026-07-04 15:14:20 +0545

    allmighty push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,7 +62,7 @@
         </div>
 
         <main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 md:py-16 lg:py-24 space-y-16 md:space-y-20 lg:space-y-24">
-
+            
             <section class="reveal">
                 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-8">
                     
